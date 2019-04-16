69dad506341aa8b19c0421285b4d6e4755ebdec9
Luan Caius <luancaius@outlook.com>
2019-04-15 22:08:04 -0400

    feat: Adding wait action

---
+++ TheActuator/Core/Services/Actions/wAIT.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Core.Services.Actions
+{
+    public class Wait : AbsAction
+    {
+        public Wait()
+        {
+            Name = "Wait";
+        }
+
+        public override void Execute(List<string> parameters)
+        {
+            var time = parameters[0];
+            Thread.sleep(time);
+        }
+    }
+}
